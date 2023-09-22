23945508ab2031c14bae4c05f966ba9407f41bb9
cj.tsai <jimtsai9596@gmail.com>
2023-09-22 09:14:07 +0800

    commit

--- 1669.cpp
+++ 1669.cpp
@@ -1,2 +1,28 @@
 #include <bits/stdc++.h>
+#define pb push_back
 using namespace std;
+
+void solve(int x){
+	i
+}
+
+void dfs(int x, int par){
+	if(pa[x]==par) return;
+	if(pa[x]) solve(x);
+
+}
+
+signed main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);
+	int n, m;cin>>n>>m;
+	vector<vector<int>> adj(n+1);
+	for(int i=0; i<m; i++){
+	 	int a, b;cin>>a>>b;
+		adj[a].pb(b);
+		adj[b].pb(a);
+	}
+	vector<int> pa(n+1, -1);
+	pa[1]=1;
+	stack<int> dfs;
+	dfs(1);
+}
--- test.cpp
+++ test.cpp
@@ -1,18 +1,10 @@
 #include <bits/stdc++.h>
-#define pii pair<int, int>
-#define int long long
-#define ff first
-#define ss second
 using namespace std;
 
-signed main(){
-	ios_base::sync_with_stdio(false);cin.tie(0);
-	cout<<0%(int)1e9+7;
-	set<int> st;
-	set.insert(asdas);
-	fdsf
-	for(int x:st){
-		cout<<x;
-	}
+int main(){
+	cout<<(6|11)<<endl;
+	cout<<(6&11)<<endl;
+	cout<<(6^11)<<endl;
+	cout<<(5<<3)<<endl;
+	cout<<(1243>>3)<<endl;
 }
-
