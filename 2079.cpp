28131734c363d3b2ca5b5ca2119d62b42e307da7
cj.tsai <jimtsai9596@gmail.com>
2023-10-16 17:20:42 +0800

    commit

--- 2079.cpp
+++ 2079.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<vector<int>> adj(200007);
-vector<int> si(200007, 0);
+vector<int> si(200007, 1);
 int root=1;
 int dfs(int x, int pa){
 	for(int d:adj[x]){
 		if(d==pa) continue;
 		si[x]+=dfs(d, x);
 	}
-	return si[x]+1;
+	return si[x];
 }
 
 signed main(){
@@ -20,10 +20,13 @@ signed main(){
 	}
 	dfs(1, 1);
 	int tmp=1;
-	bool chc=false;
+	bool chc=false;int cnt=0;
+	for(int i=1; i<=n; i++) cout<<si[i]<<' ';
+	return 0;
 	while(true){
-		//cout<<tmp<<' ';
-		if(chc) break;
+		cnt++;
+		//cout<<tmp<<' ';cout.flush();
+		//if(chc) break;
 		chc=false;
 		int tpp=n-1;
 		for(int x:adj[tmp]){
@@ -34,9 +37,23 @@ signed main(){
 				break;
 			}tpp-=si[x];
 		}
-		if(chc){
+		if(!chc||(cnt>n)){
 			cout<<tmp;
 			return 0;
 		}
 	}
 }
+
+
+/*
+1
+4    7
+     2
+	 10
+	 9
+	 8
+	 3
+	 6
+	 5
+10 7 3 1 1 2 8 4 5 6 
+ */
