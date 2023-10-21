7bd81f753db3db9c40798aea9f26e3d5e98b6de2
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2023-10-21 12:17:52 +0800

    commit

--- test.cpp
+++ test.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define int long long
+#define ff first
+#define ss second
 using namespace std;
 
-int main(){
-    while (true){
-        system("color 01");
-        system("color 12");
-        system("color 23");
-        system("color 34");
-        system("color 45");
-        system("color 56");
-        system("color 78");
-        system("color 9a");
-        system("color ab");
-        system("color bc");
-        system("color cd");
-        system("color de");
-        system("color ef");
-        system("color f0");
-    }
+signed main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);
+
+
+
+	
 }
